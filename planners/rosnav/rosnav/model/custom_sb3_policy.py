30b0322ec2cfa6cd58f4708678f3e52c10c26ae7
sunnyhello3696 <sunnyhello369@gmail.com>
2024-05-14 10:15:52 +0800

    add AGENT_97

--- planners/rosnav/rosnav/model/custom_sb3_policy.py
+++ planners/rosnav/rosnav/model/custom_sb3_policy.py
@@ -536,6 +536,27 @@ class AGENT_96(BaseAgent):
     net_arch = dict(pi=[256, 128, 64], vf=[256, 128, 64])
     activation_fn = nn.ReLU
 
+@AgentFactory.register("AGENT_97")
+class AGENT_97(BaseAgent):
+    type = PolicyType.CNN
+    space_encoder_class = ConvexMPCEncoder
+
+    # 注意顺序
+    observation_spaces = [
+        SPACE_INDEX.CONVEX,
+        # SPACE_INDEX.LASER,
+        SPACE_INDEX.GOAL,
+        SPACE_INDEX.LAST_ACTION,
+        SPACE_INDEX.LAST_ACTION_POINTS,
+    ]
+    observation_space_kwargs = {
+        "convex_map_size": 128,
+    }
+    features_extractor_class = ConvexExtractor_2d_with_ActPts
+    features_extractor_kwargs = {"features_dim": 256}
+    net_arch = dict(pi=[128, 64, 64], vf=[128, 64, 64])
+    activation_fn = nn.ReLU
+
 @AgentFactory.register("AGENT_100")
 class AGENT_100(BaseAgent):
     type = PolicyType.CNN
